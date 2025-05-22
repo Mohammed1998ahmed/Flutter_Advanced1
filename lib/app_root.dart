7c128392e25cca4a68c93d5d3ac22e44128030bb
mohammedahmeddd31234 <mohammedahmeddd31234@gmail.com>
2025-05-22 11:33:09 +0300

    _Add Splash Screen use Package . _Add File Core Router and networking an.... _Add Package ScreenUtilInit. _Add package name Project.

---
+++ lib/app_root.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'core/router/app_Route.dart';
+import 'core/router/routes.dart';
+
+class App_Root extends StatelessWidget {
+  const App_Root({super.key, required this.app_route});
+  final App_Route app_route;
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(380, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'First App',
+        // You can use the library anywhere in the app even in theme
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          scaffoldBackgroundColor: Colors.white,
+        ),
+        // home: child,
+        initialRoute: Routes.on_Obrding_page,
+        onGenerateRoute: app_route.generatorRoute,
+      ),
+    );
+  }
+}
---
+++ lib/core/router/app_Route.dart
@@ -0,0 +1,28 @@
+import 'package:advancedflutter/core/router/routes.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../feature/onbording/ui/on_bording.dart';
+
+class App_Route {
+  Route generatorRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.on_Obrding_page:
+        return MaterialPageRoute(builder: (_) => OnBording());
+
+      default:
+        return MaterialPageRoute(
+            builder: (_) => Scaffold(
+                  body: Center(
+                    child: Text(
+                      "Not Fount Page",
+                      style: TextStyle(
+                          fontSize: 18.dg,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.amber),
+                    ),
+                  ),
+                ));
+    }
+  }
+}
